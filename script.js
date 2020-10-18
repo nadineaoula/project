fe97bea3f0997928c88da18f90af2ccd412239b9
Glitch (projet-2020-partie2) <none>
2020-10-18 20:32:14 +0000

    💾😹 Updated with Glitch

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+/*
+$(document).ready(function(){
+  $("button").click(function(){
+    $.getJSON("https://data.iledefrance.fr/api/records/1.0/search/?dataset=repertoire-bibliotheques&q=&rows=279&refine.typeinst=Biblioth%C3%A8que+ou+institut+universitaire", function(result){
+      $.each(result, function(i, field){
+        $("div").append(field + " ");
+      });
+    });
+  });
+});*/
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+$.getJSON("https://data.iledefrance.fr/api/records/1.0/search/?dataset=repertoire-bibliotheques&q=&rows=279&refine.typeinst=Biblioth%C3%A8que+ou+institut+universitaire",
+function(data){
+  console.log(data);
+  
+  
+})
+  .done(function(){
+    alert("completed");
+})
+  .fail(function(e){
+    console.log("error:");
+    console(e);
+});
+  
+
